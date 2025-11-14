cb24a64b52d983e23f0496584862ba6cda4feb2b
mrandrewpettit <64383474+mrandrewpettit@users.noreply.github.com>
2025-11-13 23:19:39 -0700

    Update IntroProvider.cs

--- Jellyfin.Plugin.LocalIntros/IntroProvider.cs
+++ Jellyfin.Plugin.LocalIntros/IntroProvider.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,218 +12,212 @@ using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Entities.Movies;
 using Microsoft.Extensions.Logging;
 
-
-namespace Jellyfin.Plugin.LocalIntros;
-public class IntroProvider : IIntroProvider
+namespace Jellyfin.Plugin.LocalIntros
 {
-    private readonly ILogger<IntroProvider> logger;
-
-    public IntroProvider(ILoggerFactory loggerFactory)
+    public class IntroProvider : IIntroProvider
     {
-        logger = loggerFactory.CreateLogger<IntroProvider>();
-    }
+        private readonly ILogger<IntroProvider> logger;
 
-    public string Name { get; } = "Intros";
-
-    public Task<IEnumerable<IntroInfo>> GetIntros(BaseItem item, User user)
-    {
-        try
+        public IntroProvider(ILoggerFactory loggerFactory)
         {
+            logger = loggerFactory.CreateLogger<IntroProvider>();
+        }
 
-            if (LocalIntrosPlugin.Instance.Configuration.Local != string.Empty)
+        public string Name { get; } = "Intros";
+
+        public Task<IEnumerable<IntroInfo>> GetIntros(BaseItem item, User user)
+        {
+            try
             {
-                logger.LogTrace("Local Config Detected, retrieving local intros.");
-                return Task.FromResult(Local(item));
-            }
-            else
-            {
-                logger.LogError("No Local Config Detected, retrieving library intros.");
-                return Task.FromResult(Enumerable.Empty<IntroInfo>());
-            }
-
-        }
-        catch (Exception e)
-        {
-            logger.LogError(e, "Error retrieving intros");
-            return Task.FromResult(Enumerable.Empty<IntroInfo>());
-        }
-    }
-
-    private readonly CookieContainer _cookieContainer = new CookieContainer();
-
-    private readonly Random _random = new Random();
-
-    private static string introsPath => LocalIntrosPlugin.Instance.Configuration.Local;
-
-    private (HashSet<string> tags, HashSet<string> genres, HashSet<string> studios, DateTime now, DateTime premiereDate) GetCriteriaList(BaseItem item)
-    {
-        switch (item.GetBaseItemKind())
-        {
-            case Data.Enums.BaseItemKind.Movie:
-                var movie = item as Movie;
-
-                return (movie.Tags.ToHashSet(),movie.Genres.ToHashSet(),movie.Studios.ToHashSet(), DateTime.Now, item.PremiereDate ?? DateTime.Today);
-            case Data.Enums.BaseItemKind.Episode:
-                var episode = item as Episode;
-                var season = episode.Season;
-                var series = episode.Series;
-                return (
-                    episode.Tags.Concat(season.Tags).Concat(series.Tags).ToHashSet(),
-                    episode.Genres.Concat(season.Genres).Concat(series.Genres).ToHashSet(),
-                    episode.Studios.Concat(season.Studios).Concat(series.Studios).ToHashSet(),
-                    DateTime.Now,
-                    episode.PremiereDate ?? season.PremiereDate ?? series.PremiereDate ?? DateTime.Today
-                );
-        }
-        var emp = new HashSet<string>();
-        return (emp,emp,emp, DateTime.Now, DateTime.Today);
-    }
-
-    private IEnumerable<IntroInfo> Local(BaseItem item)
-    {
-        if (LocalIntrosPlugin.Instance.Configuration.IntrosForMoviesOnly && item.GetBaseItemKind() != Data.Enums.BaseItemKind.Movie)
-            return Enumerable.Empty<IntroInfo>();
-
-        if (!File.Exists(introsPath) && !Directory.Exists(introsPath))
-        {
-            throw new Exception("No intros found in local path");
-        }
-        var location = File.GetAttributes(introsPath);
-
-        var libraryResults = RetrieveIntroLibrary();
-
-        if (!libraryResults.Any())
-        {
-            throw new Exception("No intros found in library");
-        }
-
-        var (tags, genres, studios, now, premiereDate) = GetCriteriaList(item);
-
-        var validTagIntros = LocalIntrosPlugin.Instance.Configuration.TagIntros.Where(t => tags.Any(x => x.Equals(t.TagName, StringComparison.OrdinalIgnoreCase)));
-        var validGenreIntros = LocalIntrosPlugin.Instance.Configuration.GenreIntros.Where(g => genres.Any(x => x.Equals(g.GenreName, StringComparison.OrdinalIgnoreCase)));
-        var validStudioIntros = LocalIntrosPlugin.Instance.Configuration.StudioIntros.Where(s => studios.Any(x => x.Equals(s.StudioName, StringComparison.OrdinalIgnoreCase)));
-        var validCurrentDateIntros = LocalIntrosPlugin.Instance.Configuration.CurrentDateIntros.Where(d => d.IsDateInRange(now));
-        var validReleaseDateIntros = LocalIntrosPlugin.Instance.Configuration.PremiereDateIntros.Where(d => d.IsDateInRange(premiereDate));
-
-        FancyList<ISpecialIntro> selectableIntros = new FancyList<ISpecialIntro>();
-
-
-        selectableIntros += validTagIntros;
-        selectableIntros += validGenreIntros;
-        selectableIntros += validStudioIntros;
-        selectableIntros += validCurrentDateIntros;
-
-
-        FancyList<Guid> randomIntros = new();
-
-        if (selectableIntros.Any())
-        {
-            logger.LogInformation($"Selecting intros based on criteria, {selectableIntros.Count} intros found");
-
-            var highestPrev = selectableIntros.Max(i => i.Precedence);
-
-            var selectedIntros = selectableIntros.Where(i => i.Precedence == highestPrev);
-
-            var maxNum = selectableIntros.Sum(i => i.Prevalence);
-
-            var minNum = 0;
-
-            var index = _random.Next(minNum, maxNum);
-
-            logger.LogInformation($"Selecting intro from {minNum} to {maxNum}, selected index: {index}");
-
-            foreach (var intro in selectedIntros)
-            {
-                if (index < intro.Prevalence)
+                if (LocalIntrosPlugin.Instance.Configuration.Local != string.Empty)
                 {
-                    logger.LogInformation($"Selected intro: {intro.IntroId}");
-                    randomIntros += intro.IntroId;
-                    break;
+                    logger.LogTrace("Local Config Detected, retrieving local intros.");
+                    return Task.FromResult(Local(item));
                 }
                 else
                 {
-                    index -= intro.Prevalence;
+                    logger.LogError("No Local Config Detected, retrieving library intros.");
+                    return Task.FromResult(Enumerable.Empty<IntroInfo>());
                 }
             }
-            if (randomIntros.Count == 0)
+            catch (Exception e)
             {
-                var selItem = selectedIntros.Last();
-                logger.LogInformation($"Selected intro: {selItem.IntroId}");
-                randomIntros += selItem.IntroId;
+                logger.LogError(e, "Error retrieving intros");
+                return Task.FromResult(Enumerable.Empty<IntroInfo>());
             }
         }
-        else
+
+        private readonly CookieContainer _cookieContainer = new CookieContainer();
+
+        private readonly Random _random = new Random();
+
+        private static string introsPath => LocalIntrosPlugin.Instance.Configuration.Local;
+
+        private (HashSet<string> tags, HashSet<string> genres, HashSet<string> studios, DateTime now, DateTime premiereDate) GetCriteriaList(BaseItem item)
         {
-            randomIntros += LocalIntrosPlugin.Instance.Configuration.DefaultLocalVideos.Distinct();
-
-            logger.LogInformation($"Selecting intros based on default, {randomIntros.Count} intros found");
-        }
-        if (randomIntros.Any())
-        {
-            var selectedId = randomIntros[_random.Next(randomIntros.Count)];
-
-            logger.LogInformation($"Selected intro ID: {selectedId}");
-
-            if (libraryResults.ContainsKey(selectedId))
+            switch (item.GetBaseItemKind())
             {
-                var selectedItem = libraryResults[selectedId];
+                case Data.Enums.BaseItemKind.Movie:
+                    var movie = item as Movie;
+                    return (movie.Tags.ToHashSet(), movie.Genres.ToHashSet(), movie.Studios.ToHashSet(), DateTime.Now, item.PremiereDate ?? DateTime.Today);
 
-                logger.LogInformation($"Selected intro name: {selectedItem.Name}");
+                case Data.Enums.BaseItemKind.Episode:
+                    var episode = item as Episode;
+                    var season = episode.Season;
+                    var series = episode.Series;
+                    return (
+                        episode.Tags.Concat(season.Tags).Concat(series.Tags).ToHashSet(),
+                        episode.Genres.Concat(season.Genres).Concat(series.Genres).ToHashSet(),
+                        episode.Studios.Concat(season.Studios).Concat(series.Studios).ToHashSet(),
+                        DateTime.Now,
+                        episode.PremiereDate ?? season.PremiereDate ?? series.PremiereDate ?? DateTime.Today
+                    );
+            }
+            var emp = new HashSet<string>();
+            return (emp, emp, emp, DateTime.Now, DateTime.Today);
+        }
 
-                logger.LogInformation($"Selected intro path: {selectedItem.Path}");
+        private IEnumerable<IntroInfo> Local(BaseItem item)
+        {
+            if (LocalIntrosPlugin.Instance.Configuration.IntrosForMoviesOnly && item.GetBaseItemKind() != Data.Enums.BaseItemKind.Movie)
+                return Enumerable.Empty<IntroInfo>();
 
-                // --- BEGIN KID TAG PATCH ---
-                
-                // Make sure the returned intro has the "kid" tag for parental control
-                var updatedItem = selectedItem; // copy reference
-                if (!updatedItem.Tags.Contains("kid", StringComparer.OrdinalIgnoreCase))
+            if (!File.Exists(introsPath) && !Directory.Exists(introsPath))
+            {
+                throw new Exception("No intros found in local path");
+            }
+            var location = File.GetAttributes(introsPath);
+
+            var libraryResults = RetrieveIntroLibrary();
+
+            if (!libraryResults.Any())
+            {
+                throw new Exception("No intros found in library");
+            }
+
+            var (tags, genres, studios, now, premiereDate) = GetCriteriaList(item);
+
+            var validTagIntros = LocalIntrosPlugin.Instance.Configuration.TagIntros.Where(t => tags.Any(x => x.Equals(t.TagName, StringComparison.OrdinalIgnoreCase)));
+            var validGenreIntros = LocalIntrosPlugin.Instance.Configuration.GenreIntros.Where(g => genres.Any(x => x.Equals(g.GenreName, StringComparison.OrdinalIgnoreCase)));
+            var validStudioIntros = LocalIntrosPlugin.Instance.Configuration.StudioIntros.Where(s => studios.Any(x => x.Equals(s.StudioName, StringComparison.OrdinalIgnoreCase)));
+            var validCurrentDateIntros = LocalIntrosPlugin.Instance.Configuration.CurrentDateIntros.Where(d => d.IsDateInRange(now));
+            var validReleaseDateIntros = LocalIntrosPlugin.Instance.Configuration.PremiereDateIntros.Where(d => d.IsDateInRange(premiereDate));
+
+            FancyList<ISpecialIntro> selectableIntros = new FancyList<ISpecialIntro>();
+
+            selectableIntros += validTagIntros;
+            selectableIntros += validGenreIntros;
+            selectableIntros += validStudioIntros;
+            selectableIntros += validCurrentDateIntros;
+
+            FancyList<Guid> randomIntros = new();
+
+            if (selectableIntros.Any())
+            {
+                logger.LogInformation($"Selecting intros based on criteria, {selectableIntros.Count} intros found");
+
+                var highestPrev = selectableIntros.Max(i => i.Precedence);
+
+                var selectedIntros = selectableIntros.Where(i => i.Precedence == highestPrev);
+
+                var maxNum = selectableIntros.Sum(i => i.Prevalence);
+
+                var minNum = 0;
+
+                var index = _random.Next(minNum, maxNum);
+
+                logger.LogInformation($"Selecting intro from {minNum} to {maxNum}, selected index: {index}");
+
+                foreach (var intro in selectedIntros)
                 {
-                    var newTags = updatedItem.Tags.ToList();
-                    newTags.Add("kid");
-                    updatedItem.Tags = newTags.ToArray();
+                    if (index < intro.Prevalence)
+                    {
+                        logger.LogInformation($"Selected intro: {intro.IntroId}");
+                        randomIntros += intro.IntroId;
+                        break;
+                    }
+                    else
+                    {
+                        index -= intro.Prevalence;
+                    }
                 }
-                
-                // Return intro with "kid" tag applied
-                return new []{ new IntroInfo
+
+                if (randomIntros.Count == 0)
                 {
-                    Path = updatedItem.Path,
-                    ItemId = updatedItem.Id
-                }};
-                
-                // --- END KID TAG PATCH ---
+                    var selItem = selectedIntros.Last();
+                    logger.LogInformation($"Selected intro: {selItem.IntroId}");
+                    randomIntros += selItem.IntroId;
+                }
             }
             else
             {
-                throw new Exception($"Selected intro ID: {selectedId} not found in library");
+                randomIntros += LocalIntrosPlugin.Instance.Configuration.DefaultLocalVideos.Distinct();
+
+                logger.LogInformation($"Selecting intros based on default, {randomIntros.Count} intros found");
             }
-        }
-        return Enumerable.Empty<IntroInfo>();
-    }
 
-    private void UpdateOptionsConfig(IEnumerable<BaseItem> libraryResults)
-    {
-        // Dictionary so we can use ContainsKey
-
-        if (LocalIntrosPlugin.Instance.Configuration.DefaultLocalVideos.Count + LocalIntrosPlugin.Instance.Configuration.StudioIntros.Count + LocalIntrosPlugin.Instance.Configuration.TagIntros.Count + LocalIntrosPlugin.Instance.Configuration.GenreIntros.Count == 0)
-        {
-            LocalIntrosPlugin.Instance.Configuration.DefaultLocalVideos.Add(libraryResults.First().Id);
-        }
-        //And then to the List as we need for saving. (XML can't serialize Dictionaries..)
-        LocalIntrosPlugin.Instance.Configuration.DetectedLocalVideos = libraryResults.Select(x => new IntroVideo{
-            ItemId = x.Id,
-            Name = x.Name
-        }).OrderBy(i => i.Name).ToList();
-        LocalIntrosPlugin.Instance.SaveConfiguration();
-    }
-
-
-    private Dictionary<Guid, BaseItem> RetrieveIntroLibrary() =>
-        LocalIntrosPlugin.LibraryManager.GetItemsResult(new InternalItemsQuery
-        {
-            HasAnyProviderId = new Dictionary<string, string>
+            if (randomIntros.Any())
             {
-                {"prerolls.video", ""}
-            }
-        }).Items.ToDictionary(x => x.Id, x => x);
+                var selectedId = randomIntros[_random.Next(randomIntros.Count)];
 
+                logger.LogInformation($"Selected intro ID: {selectedId}");
+
+                if (libraryResults.ContainsKey(selectedId))
+                {
+                    var selectedItem = libraryResults[selectedId];
+
+                    logger.LogInformation($"Selected intro name: {selectedItem.Name}");
+                    logger.LogInformation($"Selected intro path: {selectedItem.Path}");
+
+                    // --- BEGIN KID TAG PATCH ---
+                    // Ensure the intro video has the "kid" tag for parental control
+                    var updatedItem = selectedItem; // Copy reference
+                    if (!updatedItem.Tags.Contains("kid", StringComparer.OrdinalIgnoreCase))
+                    {
+                        var newTags = updatedItem.Tags.ToList();  // Convert to list to modify
+                        newTags.Add("kid");                       // Add the "kid" tag
+                        updatedItem.Tags = newTags.ToArray();     // Update tags with the new list
+                    }
+                    // --- END KID TAG PATCH ---
+
+                    // Return the intro with the "kid" tag applied
+                    return new[] { new IntroInfo
+                    {
+                        Path = updatedItem.Path,
+                        ItemId = updatedItem.Id
+                    }};
+                }
+                else
+                {
+                    throw new Exception($"Selected intro ID: {selectedId} not found in library");
+                }
+            }
+            return Enumerable.Empty<IntroInfo>();
+        }
+
+        private void UpdateOptionsConfig(IEnumerable<BaseItem> libraryResults)
+        {
+            // Dictionary so we can use ContainsKey
+            if (LocalIntrosPlugin.Instance.Configuration.DefaultLocalVideos.Count + LocalIntrosPlugin.Instance.Configuration.StudioIntros.Count + LocalIntrosPlugin.Instance.Configuration.TagIntros.Count + LocalIntrosPlugin.Instance.Configuration.GenreIntros.Count == 0)
+            {
+                LocalIntrosPlugin.Instance.Configuration.DefaultLocalVideos.Add(libraryResults.First().Id);
+            }
+
+            // And then to the List as we need for saving. (XML can't serialize Dictionaries..)
+            LocalIntrosPlugin.Instance.Configuration.DetectedLocalVideos = libraryResults.Select(x => new IntroVideo{
+                ItemId = x.Id,
+                Name = x.Name
+            }).OrderBy(i => i.Name).ToList();
+            LocalIntrosPlugin.Instance.SaveConfiguration();
+        }
+
+        private Dictionary<Guid, BaseItem> RetrieveIntroLibrary() =>
+            LocalIntrosPlugin.LibraryManager.GetItemsResult(new InternalItemsQuery
+            {
+                HasAnyProviderId = new Dictionary<string, string>
+                {
+                    {"prerolls.video", ""}
+                }
+            }).Items.ToDictionary(x => x.Id, x => x);
+    }
 }
